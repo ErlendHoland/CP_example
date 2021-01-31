4353548527570a0b5235b06d1e25b22ea61392c8
Erlend <erlend990@hotmail.com>
2021-01-31 21:15:40 +0100

    init

--- StockItem.py
+++ StockItem.py
@@ -25,14 +25,7 @@ class StockItem:
 
 
 
-class StockTracker(StockItem):
-    def __init__(self, code, description, num_items):
-        self.Storage = [] 
-        super().__init__(code, description, num_items)                                        #lets stockitem class handle these paramters, so we dont need to repeat self.code, self.description etc.
-    
-    #Allows to create new stock item objects
-    def Add_StockItem(self):
-        self.Storage.append([self.code, self.description, self.num_items])
+
 
 
 
