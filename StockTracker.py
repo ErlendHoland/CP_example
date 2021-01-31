9a0b533ff2cf245e499f727ff4b0c70cc262a36a
Erlend <erlend990@hotmail.com>
2021-01-31 19:21:58 +0100

    moved class to seperate file

--- StockTracker.py
+++ StockTracker.py
@@ -1,11 +1,12 @@
-#StockTracker.py
+
+from StockItem import StockItem
 
 
-# Must contain:
-# Collection of StockItem
-# Add StockItem method
-# Find stockItem key by code
-# update stockItem amount by code
-# find stockItem by index
-
-import StockItem
+class StockTracker(StockItem):
+    def __init__(self, code, description, num_items):
+        self.Storage = [] 
+        super().__init__(code, description, num_items)                                        #lets stockitem class handle these paramters, so we dont need to repeat self.code, self.description etc.
+    
+    #Allows to create new stock item objects
+    def Add_StockItem(self):
+        self.Storage.append([self.code, self.description, self.num_items])
